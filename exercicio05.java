c26779f03575f45ac0b55a8a0ef88ac5990c3c38
João Eduardo <99699554+joaoedwin@users.noreply.github.com>
2023-04-04 12:11:26 -0300

    Update exercicio05.java

--- exercicio05.java
+++ exercicio05.java
@@ -1,3 +1,6 @@
+// Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A
+// seguir, calcule e mostre o valor da conta a pagar.
+
 import java.util.Locale;
 import java.util.Scanner;
 
